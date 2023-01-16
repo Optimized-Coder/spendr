840bae2a94f8691090b574eba1296f07c2dd2c1b
[Josh Shepherd] <[optimizedcoder.py@gmail.com]>
2023-01-16 13:25:35 +0000

    get current month from datetime module

--- core/main/routes.py
+++ core/main/routes.py
@@ -1,9 +1,16 @@
 from core.main import bp
 from flask import render_template
+from datetime import datetime
+
+today = datetime.today()
+month = today.strftime("%B")
+
+
 
 @bp.route('/')
 def index():
     return render_template(
         'index.html',
-        title = 'Spendr - Track your spending'
+        title = 'Spendr - Track your spending',
+        month = month
     )
